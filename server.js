02e3d25f5219da4843f1f07937f1d69c44e9f19b
ali-reh <alirehawi1221@gmail.com>
2025-12-27 22:42:39 +0200

    Ignore node_modules and fix entry point

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-import app from './app.js'
+import app from './app.js';
+import dotenv from 'dotenv';
 
-const port = process.env.PORT || 3000;
+dotenv.config();
 
-app.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}`);
-});
+const PORT = process.env.PORT || 3000;
+
+// Only call app.listen if we are NOT on Vercel
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
+}
+
+// Critical for Vercel
+export default app;
